9c24050c7eeead09b6f7ae969dfae0b9c78f3cde
GeorgeIfrim <georgealexandruifrim@gmail.com>
2023-04-08 17:40:15 +0300

    Initial commit

---
+++ src/main/java/com/georgeifrim/AtmMachineDemo1/AtmMachineDemo1Application.java
@@ -0,0 +1,13 @@
+package com.georgeifrim.AtmMachineDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmMachineDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmMachineDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/georgeifrim/AtmMachineDemo1/AtmMachineDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeifrim.AtmMachineDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmMachineDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
